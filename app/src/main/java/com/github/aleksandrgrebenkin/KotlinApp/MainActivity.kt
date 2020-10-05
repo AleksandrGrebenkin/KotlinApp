695fb8915969916be301aadff43943742569859d
Alexandr Grebenkin <grebyonkin.a91@gmail.com>
2020-10-05 10:55:01 +0300

    Перевел проект на Котлин

---
+++ app/src/main/java/com/github/aleksandrgrebenkin/KotlinApp/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.github.aleksandrgrebenkin.KotlinApp
+
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        button.setOnClickListener {
+            Toast.makeText(applicationContext, "I'm Clicked", Toast.LENGTH_SHORT).show()
+        }
+    }
+}
